ece1c8ecc225c9c96badd21b191bb6ee20734307
Eshah7 <shah.esha@icloud.com>
2022-07-10 11:01:08 -0400

    New Server

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -18,7 +18,7 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 
 //Dev mode
-const serverURL = "http://ec2-18-188-101-79.us-east-2.compute.amazonaws.com:3029"; //enable for dev mode
+const serverURL = "http://ec2-18-216-101-119.us-east-2.compute.amazonaws.com:3029"; //enable for dev mode
 
 //Deployment mode instructions
 //const serverURL = "http://ov-research-4.uwaterloo.ca:PORT"; //enable for deployed mode; Change PORT to the port number given to you;
