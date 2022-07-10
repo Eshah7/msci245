a9796deb058e2e52bf0044c312b754686417f531
Eshah7 <shah.esha@icloud.com>
2022-07-10 11:23:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.post('/api/getMovies', (req, res) => {
 
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
-//app.listen(port, '172.31.31.77'); //for the deployed version, specify the IP address of the server
+// app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
+app.listen(port, '172.31.31.77'); //for the deployed version, specify the IP address of the server
